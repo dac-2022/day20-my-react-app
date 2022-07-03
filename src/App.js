2026d13811939a43d539ee1d4ab5ace9d302803d
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-07-03 13:43:18 +0530

    b1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <UserAccount />
+      <hr />
+      <AddBalance />
+      <hr />
+    </div>
+  );
+}
+
+function UserAccount() {
+  let [user] = useState("Rahul");
+  let [balance] = useState(0);
+  return (
+    <div>
+      <h1>User Name - {user}</h1>
+      <h1>Address - Mumbai/Kochi</h1>
+      <h1>Mobile - 12122121</h1>
+      <h1>Balance - {balance}</h1>
+    </div>
+  );
+}
+
+function AddBalance() {
+  let [balance, setBalance] = useState(0);
+
+  const increment = () => {
+    const newcounter = balance + 1;
+    setBalance(newcounter);
+  };
+
+  return (
+    <div>
+      <h1>Account Balance</h1>
+      <h1>Balance - {balance}</h1>
+      <input type="button" value="Add/Deposit balance" onClick={increment} />
     </div>
   );
 }
